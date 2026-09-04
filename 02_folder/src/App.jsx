6aad5347c757df6898d3c33f796e14f991fb9da5
koriyanit <nitkoriya6@gmail.com>
2026-09-04 18:22:16 +0530

    4-9-2026  create the components

--- 02_folder/src/App.jsx
+++ 02_folder/src/App.jsx
@@ -1,18 +1,15 @@
-import React from 'react'
-import Card from './components/Card'
-import Navbar from './components/Navbar'
+import React from "react";
+import Card from "./components/Card";
+import "./App.css";
 
 const App = () => {
   return (
-    <div>
-      <Card  user='nit' id={23} ImgUrl= 'https://images.unsplash.com/photo-1631856395474-ddfea8b9c1e5?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDV8fHxlbnwwfHx8fHw%3D'/>
+  <div className="container">
+      <Card />
+      <Card />
+      <Card />
+  </div>
+  );
+};
 
-      <Card user='tej' id={23} ImgUrl= 'https://images.unsplash.com/photo-1606898089449-f811a830091f?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDl8fHxlbnwwfHx8fHw%3D'/>
-
-       <Card user='harshad' id={39} ImgUrl= 'https://images.unsplash.com/photo-1606898089449-f811a830091f?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDl8fHxlbnwwfHx8fHw%3D'/>
-
-    </div>
-  )
-}
-
-export default App
+export default App;
--- 02_folder/src/components/Card.jsx
+++ 02_folder/src/components/Card.jsx
@@ -1,15 +1,29 @@
-import React from 'react'
+import { BookmarkPlus } from 'lucide-react'
 
 const Card = (props) => {
-    console.log(props);
+  console.log(props);
     
   return (
-    <div className="parent">
         <div className='card'>
             <div className="top">
+              <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS5yQuKUFG8GfKlDK_F5Tfkvh2-TMw77rL5HZ9kDWjUoA&s=10' alt='logo'></img>
+              <button>Save <BookmarkPlus strokeWidth={1.75} /></button>
             </div>
-            <div className="center"></div>
-        </div>
+            <div className="center">
+                <h3>Amazon<span>% days ago</span></h3>
+                <h2>Senior Dev</h2>
+                <div>
+                  <h4>Part Time</h4>
+                  <h4>Senior Level</h4>
+                </div>
+            </div>
+            <div className="bottom">
+                <div>
+                    <h3>$120/hr</h3>
+                    <p>Mumbai, India</p>
+                </div>
+                <button>Apply Now</button>
+            </div>
     </div>
   )
 }
